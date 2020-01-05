b301e3bf52d9404c9b45918770a2c5d94a36073c
Jinten <jinten0@gmail.com>
2020-01-05 18:59:34 +0900

    adding default include path and fixing include paths

--- code/application.h
+++ code/application.h
@@ -1,9 +1,9 @@
 ﻿#pragma once
 
 #include"window.h"
-#include"../imgui/imgui.h"
-#include"../imgui/examples/imgui_impl_win32.h"
-#include"../imgui/examples/imgui_impl_dx11.h"
+#include"imgui.h"
+#include"examples/imgui_impl_win32.h"
+#include"examples/imgui_impl_dx11.h"
 
 class Application
 {
